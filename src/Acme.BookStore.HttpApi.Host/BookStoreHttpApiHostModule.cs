da80bf7b4cec42ea86e807be176c7dab5ae4581f
TranNguyenPhucAnh <phucanhatt@gmail.com>
2025-08-05 17:27:38 +0700

    - Added console writeline

--- src/Acme.BookStore.HttpApi.Host/BookStoreHttpApiHostModule.cs
+++ src/Acme.BookStore.HttpApi.Host/BookStoreHttpApiHostModule.cs
@@ -74,6 +74,13 @@ public class BookStoreHttpApiHostModule : AbpModule
 
             PreConfigure<OpenIddictServerBuilder>(serverBuilder =>
             {
+                var fileName = "mynginx.store.pfx";
+                Console.WriteLine($"Checking file: {fileName}");
+                Console.WriteLine($"File exists: {File.Exists(fileName)}");
+                Console.WriteLine($"Current directory: {Directory.GetCurrentDirectory()}");
+                Console.WriteLine($"App directory contents: {string.Join(", ", Directory.GetFiles("/app"))}");
+                Console.WriteLine($"Target directory contents: {string.Join(", ", Directory.GetFiles("/app/src/Acme.BookStore.HttpApi.Host"))}");
+                
                 // In production, it is recommended to use two RSA certificates, one for encryption, one for signing.
                 serverBuilder.AddProductionEncryptionAndSigningCertificate("mynginx.store.pfx", configuration["AuthServer:CertificatePassPhrase"]!);
                 serverBuilder.SetIssuer(new Uri(configuration["AuthServer:Authority"]!));
