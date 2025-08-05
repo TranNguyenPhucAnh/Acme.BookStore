caf3e1e0149658b22cb17b88674d08aeaa2a7fda
TranNguyenPhucAnh <phucanhatt@gmail.com>
2025-08-05 21:32:27 +0700

    - Changed add certificate method

---
+++ src/Acme.BookStore.HttpApi.Host/OpenIddictServerBuilderExtension.cs
@@ -0,0 +1,25 @@
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.Extensions.DependencyInjection;
+
+public static class OpenIddictServerBuilderExtension
+{
+    public static OpenIddictServerBuilder AddProductionEncryptionAndSigningCertificate(this OpenIddictServerBuilder builder, string fileName, string passPhrase, X509KeyStorageFlags? flag = null)
+    {
+        if (!Directory.Exists(fileName))
+        {
+            throw new FileNotFoundException($"Signing Certificate couldn't found: {fileName}");
+        }
+
+        var certificate = flag != null
+            ? X509CertificateLoader.LoadPkcs12FromFile(fileName, passPhrase, flag.Value)
+            : X509CertificateLoader.LoadPkcs12FromFile(fileName, passPhrase);
+
+        builder.AddSigningCertificate(certificate);
+        builder.AddEncryptionCertificate(certificate);
+        return builder;
+    }
+}
+
+// Note: The above code assumes that the OpenIddictServerBuilder and X509CertificateLoader classes are defined in the context of your project.
+// Ensure that you have the necessary using directives and references to use these classes correctly.
