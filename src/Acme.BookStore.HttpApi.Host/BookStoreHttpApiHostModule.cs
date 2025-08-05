5c44bad94d21a7dcbf9bd9d22e7209c0212e276d
TranNguyenPhucAnh <phucanhatt@gmail.com>
2025-08-05 17:31:10 +0700

    - Changed path to /app

--- src/Acme.BookStore.HttpApi.Host/BookStoreHttpApiHostModule.cs
+++ src/Acme.BookStore.HttpApi.Host/BookStoreHttpApiHostModule.cs
@@ -78,8 +78,6 @@ public class BookStoreHttpApiHostModule : AbpModule
                 Console.WriteLine($"Checking file: {fileName}");
                 Console.WriteLine($"File exists: {File.Exists(fileName)}");
                 Console.WriteLine($"Current directory: {Directory.GetCurrentDirectory()}");
-                Console.WriteLine($"App directory contents: {string.Join(", ", Directory.GetFiles("/app"))}");
-                Console.WriteLine($"Target directory contents: {string.Join(", ", Directory.GetFiles("/app/src/Acme.BookStore.HttpApi.Host"))}");
                 
                 // In production, it is recommended to use two RSA certificates, one for encryption, one for signing.
                 serverBuilder.AddProductionEncryptionAndSigningCertificate("mynginx.store.pfx", configuration["AuthServer:CertificatePassPhrase"]!);
