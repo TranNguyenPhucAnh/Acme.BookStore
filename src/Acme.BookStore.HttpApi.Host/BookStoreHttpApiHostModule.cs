bee719476d3958e13cb1f501d4c2865374613564
TranNguyenPhucAnh <phucanhatt@gmail.com>
2025-08-05 14:42:02 +0700

    - Added console log at certificate

--- src/Acme.BookStore.HttpApi.Host/BookStoreHttpApiHostModule.cs
+++ src/Acme.BookStore.HttpApi.Host/BookStoreHttpApiHostModule.cs
@@ -75,6 +75,9 @@ public class BookStoreHttpApiHostModule : AbpModule
             PreConfigure<OpenIddictServerBuilder>(serverBuilder =>
             {
                 // In production, it is recommended to use two RSA certificates, one for encryption, one for signing.
+                Console.WriteLine($"Looking for cert at: {"mynginx.store.pfx"}");
+                Console.WriteLine($"File.Exists = {File.Exists("mynginx.store.pfx")}");
+
                 serverBuilder.AddProductionEncryptionAndSigningCertificate("mynginx.store.pfx", configuration["AuthServer:CertificatePassPhrase"]!);
                 serverBuilder.SetIssuer(new Uri(configuration["AuthServer:Authority"]!));
                 // Increased the lifetime of authorization code and access token
