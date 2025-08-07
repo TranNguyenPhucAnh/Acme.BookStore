cb8b73fa137042cb7a485231e39715c12a7a1051
TranNguyenPhucAnh <phucanhatt@gmail.com>
2025-08-07 12:48:18 +0700

    - Added key storage flag

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo "Current user: $(id)"
-echo "Fixing permissions for /app/mynginx.store.pfx"
-chmod 644 /app/mynginx.store.pfx
-ls -l /app/mynginx.store.pfx
-echo "Starting application"
-exec dotnet Acme.BookStore.HttpApi.Host.dll
--- src/Acme.BookStore.HttpApi.Host/BookStoreHttpApiHostModule.cs
+++ src/Acme.BookStore.HttpApi.Host/BookStoreHttpApiHostModule.cs
@@ -15,6 +15,7 @@ using OpenIddict.Validation.AspNetCore;
 using System;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using Volo.Abp;
 using Volo.Abp.Account;
 using Volo.Abp.Account.Web;
@@ -76,7 +77,9 @@ public class BookStoreHttpApiHostModule : AbpModule
             {
                 // In production, it is recommended to use two RSA certificates, one for encryption, one for signing
                 OpenIddictServerBuilderExtension.AddProductionEncryptionAndSigningCertificate(
-                    serverBuilder, "mynginx.store.pfx", configuration["AuthServer:CertificatePassPhrase"]!);
+                    serverBuilder, "mynginx.store.pfx", configuration["AuthServer:CertificatePassPhrase"]!,
+                    X509KeyStorageFlags.MachineKeySet | X509KeyStorageFlags.PersistKeySet
+                    );
                 
                 serverBuilder.SetIssuer(new Uri(configuration["AuthServer:Authority"]!));
                 // Increased the lifetime of authorization code and access token
