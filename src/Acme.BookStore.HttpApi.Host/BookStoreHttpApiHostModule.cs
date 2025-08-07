3a2a1e42c5e9cf5f5cfcc697d6f39360e5593dec
TranNguyenPhucAnh <phucanhatt@gmail.com>
2025-08-07 12:53:06 +0700

    - Changed to ephemeral key set

--- src/Acme.BookStore.HttpApi.Host/BookStoreHttpApiHostModule.cs
+++ src/Acme.BookStore.HttpApi.Host/BookStoreHttpApiHostModule.cs
@@ -78,7 +78,7 @@ public class BookStoreHttpApiHostModule : AbpModule
                 // In production, it is recommended to use two RSA certificates, one for encryption, one for signing
                 OpenIddictServerBuilderExtension.AddProductionEncryptionAndSigningCertificate(
                     serverBuilder, "mynginx.store.pfx", configuration["AuthServer:CertificatePassPhrase"]!,
-                    X509KeyStorageFlags.MachineKeySet | X509KeyStorageFlags.PersistKeySet
+                    X509KeyStorageFlags.MachineKeySet | X509KeyStorageFlags.EphemeralKeySet
                     );
                 
                 serverBuilder.SetIssuer(new Uri(configuration["AuthServer:Authority"]!));
