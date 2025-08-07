aab820dc638d0fec78577bc8181e24d9e52e4f7b
TranNguyenPhucAnh <phucanhatt@gmail.com>
2025-08-07 20:25:15 +0700

    - Mounted to /tmp & removed if folder exists & copied from /tmp to /app

--- src/Acme.BookStore.HttpApi.Host/BookStoreHttpApiHostModule.cs
+++ src/Acme.BookStore.HttpApi.Host/BookStoreHttpApiHostModule.cs
@@ -77,7 +77,8 @@ public class BookStoreHttpApiHostModule : AbpModule
             {
                 // In production, it is recommended to use two RSA certificates, one for encryption, one for signing
                 OpenIddictServerBuilderExtension.AddProductionEncryptionAndSigningCertificate(
-                    serverBuilder, "mynginx.store.pfx",
+                    serverBuilder,
+                    "mynginx.store.pfx",
                     configuration["AuthServer:CertificatePassPhrase"]!, 
                     X509KeyStorageFlags.MachineKeySet | X509KeyStorageFlags.EphemeralKeySet);
                 
