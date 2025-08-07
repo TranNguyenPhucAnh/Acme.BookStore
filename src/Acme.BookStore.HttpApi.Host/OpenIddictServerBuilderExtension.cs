fc97991a0caf839331b54a41b1b818d00db2bd94
TranNguyenPhucAnh <phucanhatt@gmail.com>
2025-08-07 12:35:30 +0700

    - Added logs to code

--- src/Acme.BookStore.HttpApi.Host/OpenIddictServerBuilderExtension.cs
+++ src/Acme.BookStore.HttpApi.Host/OpenIddictServerBuilderExtension.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using System.Security.Cryptography.X509Certificates;
 using Microsoft.Extensions.DependencyInjection;
@@ -11,13 +12,36 @@ public static class OpenIddictServerBuilderExtension
         //     throw new FileNotFoundException($"Signing Certificate couldn't found: {fileName}");
         // }
 
-        var certificate = flag != null
-            ? X509CertificateLoader.LoadPkcs12FromFile(fileName, passPhrase, flag.Value)
-            : X509CertificateLoader.LoadPkcs12FromFile(fileName, passPhrase);
+        try
+            {
+                Console.WriteLine($"Attempting to load PFX file: {fileName}");
+                var certificate = flag != null
+                    ? X509CertificateLoader.LoadPkcs12FromFile(fileName, passPhrase, flag.Value)
+                    : X509CertificateLoader.LoadPkcs12FromFile(fileName, passPhrase);
+                Console.WriteLine("PFX file loaded successfully");
 
-        builder.AddSigningCertificate(certificate);
-        builder.AddEncryptionCertificate(certificate);
-        return builder;
+                Console.WriteLine("Adding signing certificate");
+                builder.AddSigningCertificate(certificate);
+                Console.WriteLine("Signing certificate added");
+
+                Console.WriteLine("Adding encryption certificate");
+                builder.AddEncryptionCertificate(certificate);
+                Console.WriteLine("Encryption certificate added");
+
+                return builder;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error in AddProductionEncryptionAndSigningCertificate: {ex}");
+                throw;
+            }
+        // var certificate = flag != null
+        //     ? X509CertificateLoader.LoadPkcs12FromFile(fileName, passPhrase, flag.Value)
+        //     : X509CertificateLoader.LoadPkcs12FromFile(fileName, passPhrase);
+
+        // builder.AddSigningCertificate(certificate);
+        // builder.AddEncryptionCertificate(certificate);
+        // return builder;
     }
 }
 
