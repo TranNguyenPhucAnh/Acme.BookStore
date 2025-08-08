e9d6ec15955932e5df3fe499f10bd12885b9beed
TranNguyenPhucAnh <phucanhatt@gmail.com>
2025-08-08 21:15:47 +0700

    - Commented add encryption certificate as unnecessary

--- src/Acme.BookStore.HttpApi.Host/OpenIddictServerBuilderExtension.cs
+++ src/Acme.BookStore.HttpApi.Host/OpenIddictServerBuilderExtension.cs
@@ -1,7 +1,6 @@
 using System;
 using System.IO;
 using System.Security.Cryptography.X509Certificates;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
 
@@ -9,7 +8,6 @@ public static class OpenIddictServerBuilderExtension
 {
     public static OpenIddictServerBuilder AddProductionEncryptionAndSigningCertificate(
         this OpenIddictServerBuilder builder,
-        IConfiguration? configuration,
         string fileName,
         string passPhrase,
         X509KeyStorageFlags? flag = null)
@@ -22,17 +20,20 @@ public static class OpenIddictServerBuilderExtension
         try
         {
             Console.WriteLine($"Attempting to load PFX file: {fileName}");
+            
             var certificate = flag != null
                 ? X509CertificateLoader.LoadPkcs12FromFile(fileName, passPhrase, flag.Value)
                 : X509CertificateLoader.LoadPkcs12FromFile(fileName, passPhrase);
 
-            // builder.AddSigningCertificate(certificate);
+            // builder.AddSigningCertificate(certificate); signing certificate ECDSA algorithm is not supported by OpenIddict
+            // If you want to use ECDSA, you need to use ECDsaSecurityKey instead of X509Certificate2, therefore credential
             // var credentials = new SigningCredentials(new ECDsaSecurityKey(certificate.GetECDsaPrivateKey())
             // {
             //     KeyId = certificate.Thumbprint
             // }, SecurityAlgorithms.EcdsaSha256);
 
             Console.WriteLine("Adding signing credential");
+
             builder.AddSigningCredentials(
             new SigningCredentials(
                 new ECDsaSecurityKey(certificate.GetECDsaPrivateKey()),
@@ -40,24 +41,24 @@ public static class OpenIddictServerBuilderExtension
 
             Console.WriteLine("Signing credential added");
 
-            Console.WriteLine("Adding encryption certificate");
+            //Console.WriteLine("Adding encryption certificate");
 
-            //actually, encrytion certificate/credential is optional, onyl add it if the client requires
-            if (!File.Exists("/app/certs/enc.pfx"))
-            {
-                throw new FileNotFoundException($"Encryption Certificate couldn't found: {"/app/certs/enc.pfx"}");
-            }
-            var encryptionCertPassword = configuration["OpenIddict:EncryptionCertificate:Password"]!;
+            //actually, encrytion certificate/credential is optional, only add it if the client requires token encryption
+            // if (!File.Exists("/app/certs/enc.pfx"))
+            // {
+            //     throw new FileNotFoundException($"Encryption Certificate couldn't found: {"/app/certs/enc.pfx"}");
+            // }
+            // var encryptionCertPassword = configuration["OpenIddict:EncryptionCertificate:Password"]!;
 
-            Console.WriteLine($"Attempting to load encyption file: /app/certs/enc.pfx");
+            // Console.WriteLine($"Attempting to load encyption file: /app/certs/enc.pfx");
 
-            var enc = flag != null
-                ? X509CertificateLoader.LoadPkcs12FromFile("/app/certs/enc.pfx", encryptionCertPassword, flag.Value)
-                : X509CertificateLoader.LoadPkcs12FromFile("/app/certs/enc.pfx", encryptionCertPassword);
+            // var enc = flag != null
+            //     ? X509CertificateLoader.LoadPkcs12FromFile("/app/certs/enc.pfx", encryptionCertPassword, flag.Value)
+            //     : X509CertificateLoader.LoadPkcs12FromFile("/app/certs/enc.pfx", encryptionCertPassword);
 
-            builder.AddEncryptionCertificate(enc);
+            // builder.AddEncryptionCertificate(enc);
 
-            Console.WriteLine("Encryption certificate added");
+            // Console.WriteLine("Encryption certificate added");
 
             return builder;
             
