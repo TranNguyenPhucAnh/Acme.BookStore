8caf63b8b6d29d3760174c6080a8c0704babb4bd
TranNguyenPhucAnh <phucanhatt@gmail.com>
2025-08-18 20:40:20 +0700

    - Uncommented forward header options

--- src/Acme.BookStore.HttpApi.Host/BookStoreHttpApiHostModule.cs
+++ src/Acme.BookStore.HttpApi.Host/BookStoreHttpApiHostModule.cs
@@ -230,11 +230,11 @@ public class BookStoreHttpApiHostModule : AbpModule
         var app = context.GetApplicationBuilder();
         var env = context.GetEnvironment();
 
-        // app.UseForwardedHeaders(new ForwardedHeadersOptions
-        //     {
-        //         ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
-        //     }
-        // );
+        app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            }
+        );
 
         if (env.IsDevelopment())
         {
