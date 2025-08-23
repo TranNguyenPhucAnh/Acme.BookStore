893eaefe7892b173009884216734a1dfa8fb9063
TranNguyenPhucAnh <phucanhatt@gmail.com>
2025-08-23 21:58:44 +0700

    - More logs

--- src/Acme.BookStore.HttpApi/Controllers/ApplicationConfigurations/BookStoreApplicationConfiguration.cs
+++ src/Acme.BookStore.HttpApi/Controllers/ApplicationConfigurations/BookStoreApplicationConfiguration.cs
@@ -73,10 +73,11 @@ namespace Acme.BookStore.Controllers.ApplicationConfigurations
             var userOrgIds = await _organizationUnitAppService.GetUserOrganizationUnitIdsAsync(CurrentUser.Id.GetValueOrDefault());
 
             Console.WriteLine($"Current User Id: {CurrentUser.Id.GetValueOrDefault()}");
-            Console.WriteLine(string.Join(", ", userOrgIds));
+            Console.WriteLine($"User Organization Unit: {string.Join(", ", userOrgIds)}");
 
             configuration.ExtraProperties["EnabledFeatures"] = features.Where(f => userOrgIds.Contains(Guid.Parse(f.Key))).Select(s => s.Name);
-            Console.WriteLine(string.Join(", ", configuration.ExtraProperties["EnabledFeatures"] as string[] ?? Array.Empty<string>()));
+
+            Console.WriteLine($"Extra Properties: {string.Join(", ", configuration.ExtraProperties["EnabledFeatures"] as string[] ?? Array.Empty<string>())}");
 
             return configuration;
         }
