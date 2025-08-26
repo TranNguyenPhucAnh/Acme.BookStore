30835cc098554abbc9e3686d95c92ab0af1c7cc0
TranNguyenPhucAnh <phucanhatt@gmail.com>
2025-08-26 13:35:04 +0700

    - Removed logs

--- src/Acme.BookStore.BackgroundWorker/BookStoreBackgroundWorker.cs
+++ src/Acme.BookStore.BackgroundWorker/BookStoreBackgroundWorker.cs
@@ -59,17 +59,7 @@ namespace Acme.BookStore.BackgroundWorker
             Logger.LogInformation($"Executed Background Worker At {utcNow}.");
 
             var schedulers = await _schedulerAppService.GetAllAsync();
-
-            var test = CronHelper.ConvertLocalCronToUtcCron(
-                schedulers.Items.First().CronExpression,
-                schedulers.Items.First().TimeZone);
-
-            Logger.LogInformation($"Local Cron: {schedulers.Items.First().CronExpression}.");
-
-            Logger.LogInformation($"UTC Cron: {test}.");
-
-            Logger.LogInformation($"Cron Tab Schedules {CrontabSchedule.Parse(test)}.");
-
+            
             var nextOccurences = schedulers.Items
                 .SelectMany(s => CrontabSchedule.Parse(CronHelper.ConvertLocalCronToUtcCron(s.CronExpression, s.TimeZone))
                 .GetNextOccurrences(utcNow.AddSeconds(-1), DateTime.UtcNow.AddMonths(1))
