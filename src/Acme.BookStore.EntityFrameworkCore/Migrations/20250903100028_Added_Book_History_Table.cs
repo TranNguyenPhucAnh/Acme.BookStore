5a4105e2c35de36ce7390e5517ade9fd5c62b2a4
TranNguyenPhucAnh <phucanhatt@gmail.com>
2025-09-03 18:42:52 +0700

    - Edited add trigger syntax

--- src/Acme.BookStore.EntityFrameworkCore/Migrations/20250903100028_Added_Book_History_Table.cs
+++ src/Acme.BookStore.EntityFrameworkCore/Migrations/20250903100028_Added_Book_History_Table.cs
@@ -38,87 +38,75 @@ namespace Acme.BookStore.Migrations
                 .Annotation("MySql:CharSet", "utf8mb4");
 
             migrationBuilder.Sql(@"
-                DELIMITER $$
-
-                    CREATE TRIGGER book_after_insert
-                    AFTER INSERT ON Book
-                    FOR EACH ROW
-                    BEGIN
-                        INSERT INTO BookHistory (
-                            Id, BookId, ISBN, Name, Type, PublishDate, Publisher, AuthorId,
-                            ActionType, PreviousAuditId, Version, CreationTime
-                        )
-                        VALUES (
-                            UUID(), NEW.Id, NEW.ISBN, NEW.Name, NEW.Type, NEW.PublishDate, NEW.Publisher, NEW.AuthorId,
-                            0, NULL, 0, NOW()
-                        );
-                    END$$
-
-                DELIMITER ;
+                CREATE TRIGGER `book_after_insert`
+                AFTER INSERT ON `Book`
+                FOR EACH ROW
+                BEGIN
+                    INSERT INTO BookHistory (
+                        Id, BookId, ISBN, Name, Type, PublishDate, Publisher, AuthorId,
+                        ActionType, PreviousAuditId, Version, CreationTime
+                    )
+                    VALUES (
+                        UUID(), NEW.Id, NEW.ISBN, NEW.Name, NEW.Type, NEW.PublishDate, NEW.Publisher, NEW.AuthorId,
+                        0, NULL, 0, NOW()
+                    );
+                END
             ");
 
             migrationBuilder.Sql(@"
-                DELIMITER $$
+                CREATE TRIGGER `book_after_update`
+                AFTER UPDATE ON `Book`
+                FOR EACH ROW
+                BEGIN
+                    DECLARE lastVersion INT;
+                    DECLARE lastAuditId CHAR(36);
 
-                    CREATE TRIGGER book_after_update
-                    AFTER UPDATE ON Book
-                    FOR EACH ROW
-                    BEGIN
-                        DECLARE lastVersion INT;
-                        DECLARE lastAuditId CHAR(36);
+                    -- Lấy version và Id của bản ghi history mới nhất
+                    SELECT Version, Id
+                    INTO lastVersion, lastAuditId
+                    FROM BookHistory
+                    WHERE BookId = NEW.Id
+                    ORDER BY Version DESC
+                    LIMIT 1;
 
-                        -- Lấy version và Id của bản ghi history mới nhất
-                        SELECT Version, Id
-                        INTO lastVersion, lastAuditId
-                        FROM BookHistory
-                        WHERE BookId = NEW.Id
-                        ORDER BY Version DESC
-                        LIMIT 1;
-
-                        -- Thêm bản ghi mới
-                        INSERT INTO BookHistory (
-                            Id, BookId, ISBN, Name, Type, PublishDate, Publisher, AuthorId,
-                            ActionType, PreviousAuditId, Version, CreationTime
-                        )
-                        VALUES (
-                            UUID(), NEW.Id, NEW.ISBN, NEW.Name, NEW.Type, NEW.PublishDate, NEW.Publisher, NEW.AuthorId,
-                            1, lastAuditId, lastVersion + 1, NOW()
-                        );
-                    END$$
-
-                DELIMITER ;
+                    -- Thêm bản ghi mới
+                    INSERT INTO BookHistory (
+                        Id, BookId, ISBN, Name, Type, PublishDate, Publisher, AuthorId,
+                        ActionType, PreviousAuditId, Version, CreationTime
+                    )
+                    VALUES (
+                        UUID(), NEW.Id, NEW.ISBN, NEW.Name, NEW.Type, NEW.PublishDate, NEW.Publisher, NEW.AuthorId,
+                        1, lastAuditId, lastVersion + 1, NOW()
+                    );
+                END
             ");
 
             migrationBuilder.Sql(@"
-                DELIMITER $$
+                CREATE TRIGGER `book_after_delete`
+                AFTER DELETE ON `Book`
+                FOR EACH ROW
+                BEGIN
+                    DECLARE lastVersion INT;
+                    DECLARE lastAuditId CHAR(36);
 
-                    CREATE TRIGGER book_after_delete
-                    AFTER DELETE ON Book
-                    FOR EACH ROW
-                    BEGIN
-                        DECLARE lastVersion INT;
-                        DECLARE lastAuditId CHAR(36);
+                    -- Lấy version và Id của bản ghi history mới nhất
+                    SELECT Version, Id
+                    INTO lastVersion, lastAuditId
+                    FROM BookHistory
+                    WHERE BookId = OLD.Id
+                    ORDER BY Version DESC
+                    LIMIT 1;
 
-                        -- Lấy version và Id của bản ghi history mới nhất
-                        SELECT Version, Id
-                        INTO lastVersion, lastAuditId
-                        FROM BookHistory
-                        WHERE BookId = OLD.Id
-                        ORDER BY Version DESC
-                        LIMIT 1;
-
-                        -- Thêm bản ghi mới để đánh dấu Delete
-                        INSERT INTO BookHistory (
-                            Id, BookId, ISBN, Name, Type, PublishDate, Publisher, AuthorId,
-                            ActionType, PreviousAuditId, Version, CreationTime
-                        )
-                        VALUES (
-                            UUID(), OLD.Id, OLD.ISBN, OLD.Name, OLD.Type, OLD.PublishDate, OLD.Publisher, OLD.AuthorId,
-                            2, lastAuditId, COALESCE(lastVersion, -1) + 1, NOW()
-                        );
-                    END$$
-
-                DELIMITER ;
+                    -- Thêm bản ghi mới để đánh dấu Delete
+                    INSERT INTO BookHistory (
+                        Id, BookId, ISBN, Name, Type, PublishDate, Publisher, AuthorId,
+                        ActionType, PreviousAuditId, Version, CreationTime
+                    )
+                    VALUES (
+                        UUID(), OLD.Id, OLD.ISBN, OLD.Name, OLD.Type, OLD.PublishDate, OLD.Publisher, OLD.AuthorId,
+                        2, lastAuditId, COALESCE(lastVersion, -1) + 1, NOW()
+                    );
+                END
             ");
         }
 
