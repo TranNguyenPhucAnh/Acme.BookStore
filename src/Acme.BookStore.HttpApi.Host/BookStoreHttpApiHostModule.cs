614d24c3565a437e6621361039d3a829c183cb4e
TranNguyenPhucAnh <93979599+TranNguyenPhucAnh@users.noreply.github.com>
2025-10-14 13:26:16 +0700

    Update BookStoreHttpApiHostModule.cs

--- src/Acme.BookStore.HttpApi.Host/BookStoreHttpApiHostModule.cs
+++ src/Acme.BookStore.HttpApi.Host/BookStoreHttpApiHostModule.cs
@@ -81,7 +81,7 @@ public class BookStoreHttpApiHostModule : AbpModule
             PreConfigure<OpenIddictServerBuilder>(serverBuilder =>
             {
                 // In production, it is recommended to use two RSA certificates, one for encryption, one for signing
-                // OpenIddictServerBuilderExtension.AddProductionEncryptionAndSigningCertificate(serverBuilder, awsSecrets, configuration);
+                OpenIddictServerBuilderExtension.AddProductionEncryptionAndSigningCertificate(serverBuilder, awsSecrets, configuration);
 
                 serverBuilder.SetIssuer(new Uri(configuration["AuthServer:Authority"]!));
                 // Increased the lifetime of authorization code and access token
